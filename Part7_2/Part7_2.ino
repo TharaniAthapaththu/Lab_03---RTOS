7c7553a01718fc1b4140525cd5ef271043606d0c
TharaniAthapaththu <104664951+TharaniAthapaththu@users.noreply.github.com>
2022-05-07 23:35:22 +0530

    Add files via upload

--- Part7_2/Part7_2.ino
+++ Part7_2/Part7_2.ino
@@ -3,7 +3,26 @@
 const char* ssid     = "EN20412098";
 const char* password = "Tharani@123";
 
-void C1Task(void *parameters) {
+WiFiServer server(80);
+String header;
+
+String LED_1_State = "off";
+String LED_2_State = "off";
+
+static const int LED_1= 5;
+static const int LED_2 = 6;
+
+// Current time
+unsigned long currentTime = millis();
+// Previous time
+unsigned long previousTime = 0;
+// Define timeout time in milliseconds (example: 2000ms = 2s)
+const long timeoutTime = 2000;
+
+static SemaphoreHandle_t mutex;
+
+
+void Task_A(void *parameters) {
   WiFi.begin(ssid, password);
   Serial.print("Connecting to ");
   Serial.print(ssid);
@@ -24,22 +43,131 @@ void C1Task(void *parameters) {
   vTaskDelete(NULL);
 }
 
-void C2Task(void *parameters) {
+void Task_B(void *parameters) {
   
+  while(1){
+    if(xSemaphoreTake(mutex,0) == pdTRUE){
+    WiFiClient client = server.available();   // Listen for incoming clients
+  
+    if (client) {                             // If a new client connects,
+      currentTime = millis();
+      previousTime = currentTime;
+      Serial.println("New Client.");          // print a message out in the serial port
+      String currentLine = "";                // make a String to hold incoming data from the client
+      while (client.connected() && currentTime - previousTime <= timeoutTime) {  // loop while the client's connected
+        currentTime = millis();
+        if (client.available()) {             // if there's bytes to read from the client,
+          char c = client.read();             // read a byte, then
+          Serial.write(c);                    // print it out the serial monitor
+          header += c;
+          if (c == '\n') {                    // if the byte is a newline character
+            // if the current line is blank, you got two newline characters in a row.
+            // that's the end of the client HTTP request, so send a response:
+            if (currentLine.length() == 0) {
+              // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+              // and a content-type so the client knows what's coming, then a blank line:
+              client.println("HTTP/1.1 200 OK");
+              client.println("Content-type:text/html");
+              client.println("Connection: close");
+              client.println();
+  
+              // turns the GPIOs on and off
+              if (header.indexOf("GET /5/on") >= 0) {
+                Serial.println("GPIO 5 on");
+                LED_1_State = "on";
+                digitalWrite(LED_1, HIGH);
+              } else if (header.indexOf("GET /5/off") >= 0) {
+                Serial.println("GPIO 5 off");
+                LED_1_State = "off";
+                digitalWrite(LED_1, LOW);
+                Serial.println("GPIO 6 on");
+              } else if (header.indexOf("GET /6/on") >= 0) {
+                LED_2_State = "on";
+                digitalWrite(LED_2, HIGH);
+              } else if (header.indexOf("GET /6/off") >= 0) {
+                Serial.println("GPIO 6 off");
+                LED_2_State = "off";
+                digitalWrite(LED_2, LOW);
+              }
+  
+              // Display the HTML web page
+              client.println("<!DOCTYPE html><html>");
+              client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+              client.println("<link rel=\"icon\" href=\"data:,\">");
+              // CSS to style the on/off buttons
+              // Feel free to change the background-color and font-size attributes to fit your preferences
+              client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+              client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
+              client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+              client.println(".button2 {background-color: #555555;}</style></head>");
+  
+              // Web Page Heading
+              client.println("<body><h1>ESP32 Web Server</h1>");
+  
+              // Display current state, and ON/OFF buttons for GPIO 26
+              client.println("<p>GPIO 5 - State " + LED_1_State + "</p>");
+              // If the output26State is off, it displays the ON button
+              if (LED_1_State == "off") {
+                client.println("<p><a href=\"/5/on\"><button class=\"button\">ON</button></a></p>");
+              } else {
+                client.println("<p><a href=\"/5/off\"><button class=\"button button2\">OFF</button></a></p>");
+              }
+  
+              // Display current state, and ON/OFF buttons for GPIO 27
+              client.println("<p>GPIO 6 - State " + LED_2_State + "</p>");
+              // If the output27State is off, it displays the ON button
+              if (LED_2_State == "off") {
+                client.println("<p><a href=\"/6/on\"><button class=\"button\">ON</button></a></p>");
+              } else {
+                client.println("<p><a href=\"/6/off\"><button class=\"button button2\">OFF</button></a></p>");
+              }
+              client.println("</body></html>");
+  
+              // The HTTP response ends with another blank line
+              client.println();
+              // Break out of the while loop
+              break;
+            } else { // if you got a newline, then clear currentLine
+              currentLine = "";
+            }
+          } else if (c != '\r') {  // if you got anything else but a carriage return character,
+            currentLine += c;      // add it to the end of the currentLine
+          }
+        }
+      }
+      // Clear the header variable
+      header = "";
+      // Close the connection
+      client.stop();
+      Serial.println("Client disconnected.");
+      Serial.println("");
+    }
+    xSemaphoreGive(mutex);
+  }else{
+
+ }
+}
 }
 
 void setup() {
   Serial.begin(115200);
+  
+  pinMode(LED_1, OUTPUT);
+  pinMode(LED_2, OUTPUT);
+
+  digitalWrite(LED_1, LOW);
+  digitalWrite(LED_2, LOW);
+  
   delay(10);
   Serial.println('\n');
+
+  mutex = xSemaphoreCreateMutex();
+ 
   
-  xTaskCreatePinnedToCore(C1Task,
-                          "Task A Core0",
-                          4096,
-                          NULL,
-                          1,
-                          NULL,
-                          0);
+  xTaskCreatePinnedToCore(Task_A, "Task A", 5000, NULL, 1, NULL, 0);
+  xTaskCreatePinnedToCore(Task_B, "Task A", 1024, NULL, 1, NULL, 1);
+
+vTaskDelete(NULL);
 }
 
 void loop() { 
